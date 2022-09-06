7cd787096648c1280e97fe5183773684dc016769
Philippe L'ATTENTION <philippe.lattention@hotmail.fr>
2022-09-06 21:40:22 +0400

    add importing prisma package from remix-app

---
+++ apps/remix-app/app/db.server.ts
@@ -0,0 +1,2 @@
+import { prisma } from "database";
+export default prisma;
--- apps/remix-app/app/routes/index.tsx
+++ apps/remix-app/app/routes/index.tsx
@@ -1,23 +1,30 @@
+import type { LoaderArgs } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import prisma from "~/db.server";
+
+export const loader = async ({ request }: LoaderArgs) => {
+  const users = await prisma.user.findMany();
+  return json({ users });
+};
+
 export default function Index() {
+  const { users } = useLoaderData<typeof loader>();
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
+      <h2>Users from database</h2>
+      {users.map((user) => (
+        <div>{JSON.stringify(user)}</div>
+      ))}
       <ul>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
+          <a target="_blank" href="https://remix.run/tutorials/blog" rel="noreferrer">
             15m Quickstart Blog Tutorial
           </a>
         </li>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
+          <a target="_blank" href="https://remix.run/tutorials/jokes" rel="noreferrer">
             Deep Dive Jokes App Tutorial
           </a>
         </li>
