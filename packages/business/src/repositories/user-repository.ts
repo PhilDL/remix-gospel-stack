974eedadfbe71143e0c03070f0e95baeec9d96cd
Philippe L'ATTENTION <philippe.lattention@hotmail.fr>
2023-04-06 08:36:13 +0400

    fix tsyringe TypeScript 5.0 undefined inject type

--- packages/business/src/repositories/user-repository.ts
+++ packages/business/src/repositories/user-repository.ts
@@ -1,11 +1,13 @@
-import { autoInjectable, inject } from "tsyringe";
+import { autoInjectable, container } from "tsyringe";
 import type { PrismaClient } from "@remix-gospel-stack/database";
 import type { UserRepository } from "./iuser-repository";
 import type { User } from "../shared/dtos";
 
 @autoInjectable()
 export class PrismaUserRepository implements UserRepository {
-  constructor(@inject("PrismaClient") private readonly prisma: PrismaClient) {}
+  constructor(private readonly prisma: PrismaClient) {
+    this.prisma = container.resolve("PrismaClient");
+  }
   async getUsers(): Promise<User[]> {
     return this.prisma.user.findMany();
   }
