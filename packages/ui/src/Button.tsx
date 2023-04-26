9df1a923d71000df899d218ad79e07172ee6f16f
Philippe L'ATTENTION <philippe.lattention@hotmail.fr>
2023-04-26 08:41:21 +0400

    fix build

--- packages/ui/src/Button.tsx
+++ packages/ui/src/Button.tsx
@@ -4,7 +4,7 @@ export const Button = () => {
   return (
     <div className="rounded-md ">
       <a href="https://turborepo.org/docs/getting-started" role="button">
-        <div className="flex w-full items-center justify-center rounded-md border border-transparent bg-orange-500 px-8 py-3 text-base font-medium text-white no-underline hover:bg-gray-700  md:py-3 md:px-10 md:text-lg md:leading-6">
+        <div className="w-full flex items-center justify-center rounded-md border border-transparent bg-orange-500 px-8 py-3 text-base font-medium text-white no-underline hover:bg-gray-700  md:py-3 md:px-10 md:text-lg md:leading-6">
           From "ui" package
           <span className="ml-2 bg-gradient-to-r from-brandred to-brandblue bg-clip-text text-transparent animate-pulse">
             →
