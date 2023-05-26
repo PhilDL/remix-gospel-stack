3d45bc805894989c8d265337080b194ae15e0df5
Philippe L'ATTENTION <philippe.lattention@hotmail.fr>
2023-05-26 18:02:57 +0400

    fly memory issues

--- apps/remix-app/scripts/migrate.sh
+++ apps/remix-app/scripts/migrate.sh
@@ -1,15 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 set -ex
 
 # Setup 512MB of space for swap and set permissions and turn on swapmode
-fallocate -l 512M /swapfile
+fallocate -l 512MB /swapfile
 chmod 0600 /swapfile
 mkswap /swapfile
 echo 10 > /proc/sys/vm/swappiness
 swapon /swapfile
 
-npx prisma migrate deploy --schema packages/database/prisma/schema.prisma
-
-swapoff /swapfile
-rm /swapfile
+npx --yes prisma migrate deploy --schema packages/database/prisma/schema.prisma
