172932241eb1db4d0d14ffc2d5ab59424f3cb240
Philippe L'ATTENTION <philippe.lattention@hotmail.fr>
2023-05-26 18:22:56 +0400

    fly.io swap mem setup

--- apps/remix-app/scripts/migrate.sh
+++ apps/remix-app/scripts/migrate.sh
@@ -3,10 +3,10 @@
 set -ex
 
 # Setup 512MB of space for swap and set permissions and turn on swapmode
-# fallocate -l 512MB /swapfile
-# chmod 0600 /swapfile
-# mkswap /swapfile
-# echo 10 > /proc/sys/vm/swappiness
-# swapon /swapfile
+fallocate -l 512MB /swapfile
+chmod 0600 /swapfile
+mkswap /swapfile
+echo 10 > /proc/sys/vm/swappiness
+swapon /swapfile
 
 npx --yes prisma migrate deploy --schema packages/database/prisma/schema.prisma
