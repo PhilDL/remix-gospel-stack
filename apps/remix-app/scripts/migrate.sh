87a7ca491f94b5447390904e49cf0597551bbd52
Philippe L'ATTENTION <philippe.lattention@hotmail.fr>
2023-05-26 10:29:07 +0400

    OOM fly.io

--- apps/remix-app/scripts/migrate.sh
+++ apps/remix-app/scripts/migrate.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-fallocate -l 512M /swapfile
+fallocate -l 256M /swapfile
 chmod 0600 /swapfile
 mkswap /swapfile
 echo 10 >/proc/sys/vm/swappiness
 swapon /swapfile
 echo 1 >/proc/sys/vm/overcommit_memory
-npx prisma migrate deploy
+npx prisma migrate deploy --schema packages/database/prisma/schema.prisma
+swapoff /swapfile
+rm /swapfile
