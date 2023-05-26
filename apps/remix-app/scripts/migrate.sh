9c95558dda9328decb04fb3a7c66946b064e8ae6
Philippe L'ATTENTION <philippe.lattention@hotmail.fr>
2023-05-26 10:37:37 +0400

    fixing fly deploy

--- apps/remix-app/scripts/migrate.sh
+++ apps/remix-app/scripts/migrate.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-fallocate -l 256M /swapfile
+set -ex
+
+# Setup 512MB of space for swap and set permissions and turn on swapmode
+fallocate -l 512M /swapfile
 chmod 0600 /swapfile
 mkswap /swapfile
-echo 10 >/proc/sys/vm/swappiness
+echo 10 > /proc/sys/vm/swappiness
 swapon /swapfile
-echo 1 >/proc/sys/vm/overcommit_memory
+
 npx prisma migrate deploy --schema packages/database/prisma/schema.prisma
+
 swapoff /swapfile
 rm /swapfile
