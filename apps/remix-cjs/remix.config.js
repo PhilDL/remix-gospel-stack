b49a80191887e866b53745c147443d103a4b9d9f
Philippe L'ATTENTION <philippe.lattention@hotmail.fr>
2023-09-03 12:05:13 +0400

    add example for remix vercel cjs app

---
+++ apps/remix-cjs/remix.config.js
@@ -0,0 +1,28 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  ignoredRouteFiles: ["**/.*"],
+  // When running locally in development mode, we use the built-in remix
+  // server. This does not understand the vercel lambda module format,
+  // so we default back to the standard build output.
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.ts",
+  serverBuildPath: "api/index.js",
+  watchPaths: [
+    "../../packages/ui/src/**/*",
+    "../../packages/internal-nobuild/src/**/*",
+  ],
+  serverDependenciesToBundle: [],
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // publicPath: "/build/",
+  serverModuleFormat: "cjs",
+  tailwind: true,
+  postcss: true,
+  future: {
+    v2_dev: true,
+    v2_errorBoundary: true,
+    v2_headers: true,
+    v2_meta: true,
+    v2_normalizeFormMethod: true,
+    v2_routeConvention: true,
+  },
+};
