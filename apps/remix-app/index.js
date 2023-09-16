7aa3ad5d0495199b676060a7d9c8eeca15e83920
Philippe L'ATTENTION <philippe.lattention@hotmail.fr>
2023-09-16 19:13:24 +0400

    ...

--- apps/remix-app/index.js
+++ apps/remix-app/index.js
@@ -1,8 +1,12 @@
 import "dotenv/config";
 
+import { installGlobals } from "@remix-run/node";
+
 // if (process.env.MOCKS === "true") {
 //   await import("./tests/mocks/index.ts");
 // }
+
+installGlobals();
 
 if (process.env.NODE_ENV === "production") {
   await import("./server-build/index.js");
