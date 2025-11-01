3c9bbe84a36a3febd1ec2b8e9ae4466febbd3b8d
Philippe L'ATTENTION <philippe.lattention@hotmail.fr>
2025-11-01 22:37:29 +0400

    delete dev log

--- apps/webapp/app/db.server.ts
+++ apps/webapp/app/db.server.ts
@@ -5,11 +5,6 @@ import { createClient } from "@react-router-gospel-stack/database";
 import { env } from "./env.server";
 
 export const db = remember("db", () => {
-  console.log({
-    url: env.DATABASE_URL,
-    authToken: env.DATABASE_AUTH_TOKEN,
-    syncUrl: env.DATABASE_SYNC_URL,
-  });
   return createClient({
     url: env.DATABASE_URL,
     authToken: env.DATABASE_AUTH_TOKEN,
